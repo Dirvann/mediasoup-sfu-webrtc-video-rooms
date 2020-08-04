ca3563149005cdc34fc150437c622f2258204b91
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2020-08-04 14:36:04 +0200

    added input selector, ui improvement

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 if (location.href.substr(0, 5) !== 'https')
-    location.href = 'https' + location.href.substr(4, location.href.length - 4)
+  location.href = 'https' + location.href.substr(4, location.href.length - 4)
 
 const socket = io()
 
@@ -9,88 +9,106 @@ let producer = null;
 nameInput.value = 'bob' + Math.round(Math.random() * 1000)
 
 socket.on('reload', () => {
-    location = location
+  location = location
 })
 socket.request = function request(type, data = {}) {
-    return new Promise((resolve, reject) => {
-        socket.emit(type, data, (data) => {
-            if (data.error) {
-                reject(data.error)
-            } else {
-                resolve(data)
-            }
-        })
+  return new Promise((resolve, reject) => {
+    socket.emit(type, data, (data) => {
+      if (data.error) {
+        reject(data.error)
+      } else {
+        resolve(data)
+      }
     })
+  })
 }
 
 let rc = null
 
 function joinRoom(name, room_id) {
-    if(rc && rc.isOpen()) {
-        console.log('already connected to a room')
-    } else {
-        rc = new RoomClient(localMedia, remoteVideos, remoteAudios, window.mediasoupClient, socket, room_id, name, roomOpen)
+  if (rc && rc.isOpen()) {
+    console.log('already connected to a room')
+  } else {
+    rc = new RoomClient(localMedia, remoteVideos, remoteAudios, window.mediasoupClient, socket, room_id, name, roomOpen)
 
-        addListeners()
-    }
-    
+    addListeners()
+  }
+
 }
 
 function roomOpen() {
-    login.className = 'hidden'
-    reveal(startAudioButton)
-    hide(stopAudioButton)
-    reveal(startVideoButton)
-    hide(stopVideoButton)
-    reveal(startScreenButton)
-    hide(stopScreenButton)
-    reveal(exitButton)
-    control.className = ''
+  login.className = 'hidden'
+  reveal(startAudioButton)
+  hide(stopAudioButton)
+  reveal(startVideoButton)
+  hide(stopVideoButton)
+  reveal(startScreenButton)
+  hide(stopScreenButton)
+  reveal(exitButton)
+  control.className = ''
+  reveal(videoMedia)
 }
+
 function hide(elem) {
-    elem.className = 'hidden'
+  elem.className = 'hidden'
 }
+
 function reveal(elem) {
-    elem.className = ''
+  elem.className = ''
 }
 
 
 function addListeners() {
-    rc.on(RoomClient.EVENTS.startScreen, () => {
-        hide(startScreenButton)
-        reveal(stopScreenButton)
-    })
+  rc.on(RoomClient.EVENTS.startScreen, () => {
+    hide(startScreenButton)
+    reveal(stopScreenButton)
+  })
 
-    rc.on(RoomClient.EVENTS.stopScreen, () => {
-        hide(stopScreenButton)
-        reveal(startScreenButton)
-        
-    })
+  rc.on(RoomClient.EVENTS.stopScreen, () => {
+    hide(stopScreenButton)
+    reveal(startScreenButton)
 
-    rc.on(RoomClient.EVENTS.stopAudio, () => {
-        hide(stopAudioButton)
-        reveal(startAudioButton)
-        
-    })
-    rc.on(RoomClient.EVENTS.startAudio, () => {
-        hide(startAudioButton)
-        reveal(stopAudioButton) 
-    })
+  })
 
-    rc.on(RoomClient.EVENTS.startVideo, () => {
-        hide(startVideoButton)
-        reveal(stopVideoButton) 
-    })
-    rc.on(RoomClient.EVENTS.stopVideo, () => {
-        hide(stopVideoButton)
-        reveal(startVideoButton) 
-    })
-    rc.on(RoomClient.EVENTS.exitRoom, () => {
-        hide(control)
-        reveal(login)
-    })
+  rc.on(RoomClient.EVENTS.stopAudio, () => {
+    hide(stopAudioButton)
+    reveal(startAudioButton)
+
+  })
+  rc.on(RoomClient.EVENTS.startAudio, () => {
+    hide(startAudioButton)
+    reveal(stopAudioButton)
+  })
+
+  rc.on(RoomClient.EVENTS.startVideo, () => {
+    hide(startVideoButton)
+    reveal(stopVideoButton)
+  })
+  rc.on(RoomClient.EVENTS.stopVideo, () => {
+    hide(stopVideoButton)
+    reveal(startVideoButton)
+  })
+  rc.on(RoomClient.EVENTS.exitRoom, () => {
+    hide(control)
+    reveal(login)
+    hide(videoMedia)
+  })
 }
 
+// Load mediaDevice options
+navigator.mediaDevices.enumerateDevices().then(devices =>
+  devices.forEach(device => {
+    let el = null
+    if ('audioinput' === device.kind) {
+      el = audioSelect
+    } else if ('videoinput' === device.kind) {
+      el = videoSelect
+    }
+    if(!el) return
 
-
-
+    let option = document.createElement('option')
+    option.value = device.deviceId
+    option.innerText = device.label
+    el.appendChild(option)
+  })
+)
