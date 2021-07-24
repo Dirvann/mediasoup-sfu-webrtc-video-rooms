da0dc215c4b65ba6dd1209f70d9fd31c82fc8248
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2021-07-24 12:28:08 +0200

    refactoring

--- src/Peer.js
+++ src/Peer.js
@@ -1,21 +1,20 @@
 module.exports = class Peer {
     constructor(socket_id, name) {
-        this.id = socket_id
-        this.name = name
-        this.transports = new Map()
-        this.consumers = new Map()
-        this.producers = new Map()
+        this.id = socket_id;
+        this.name = name;
+        this.transports = new Map();
+        this.consumers = new Map();
+        this.producers = new Map();
     }
 
-
     addTransport(transport) {
-        this.transports.set(transport.id, transport)
+        this.transports.set(transport.id, transport);
     }
 
     async connectTransport(transport_id, dtlsParameters) {
-        if (!this.transports.has(transport_id)) return
+        if (!this.transports.has(transport_id)) return;
         await this.transports.get(transport_id).connect({
-            dtlsParameters: dtlsParameters
+            dtlsParameters: dtlsParameters,
         });
     }
 
@@ -23,51 +22,54 @@ module.exports = class Peer {
         //TODO handle null errors
         let producer = await this.transports.get(producerTransportId).produce({
             kind,
-            rtpParameters
-        })
+            rtpParameters,
+        });
 
-        this.producers.set(producer.id, producer)
+        this.producers.set(producer.id, producer);
 
-        producer.on('transportclose', function() {
-            console.log(`---producer transport close--- name: ${this.name} consumer_id: ${producer.id}`)
-            producer.close()
-            this.producers.delete(producer.id)
-            
-        }.bind(this))
+        producer.on(
+            'transportclose',
+            function () {
+                console.log('Producer transport close', { name: `${this.name} consumer_id: ${producer.id}`});
+                producer.close();
+                this.producers.delete(producer.id);
+            }.bind(this),
+        );
 
-        return producer
+        return producer;
     }
 
     async createConsumer(consumer_transport_id, producer_id, rtpCapabilities) {
-        let consumerTransport = this.transports.get(consumer_transport_id)
+        let consumerTransport = this.transports.get(consumer_transport_id);
 
-        let consumer = null
+        let consumer = null;
         try {
             consumer = await consumerTransport.consume({
                 producerId: producer_id,
                 rtpCapabilities,
-                paused: false //producer.kind === 'video',
+                paused: false, //producer.kind === 'video',
             });
         } catch (error) {
-            console.error('consume failed', error);
+            console.error('Consume failed', error);
             return;
         }
 
         if (consumer.type === 'simulcast') {
             await consumer.setPreferredLayers({
                 spatialLayer: 2,
-                temporalLayer: 2
+                temporalLayer: 2,
             });
         }
 
-        this.consumers.set(consumer.id, consumer)
+        this.consumers.set(consumer.id, consumer);
 
-        consumer.on('transportclose', function() {
-            console.log(`---consumer transport close--- name: ${this.name} consumer_id: ${consumer.id}`)
-            this.consumers.delete(consumer.id)
-        }.bind(this))
-
-        
+        consumer.on(
+            'transportclose',
+            function () {
+                console.log('Consumer transport close', { name: `${this.name} consumer_id: ${consumer.id}`});
+                this.consumers.delete(consumer.id);
+            }.bind(this),
+        );
 
         return {
             consumer,
@@ -77,32 +79,30 @@ module.exports = class Peer {
                 kind: consumer.kind,
                 rtpParameters: consumer.rtpParameters,
                 type: consumer.type,
-                producerPaused: consumer.producerPaused
-            }
-        }
+                producerPaused: consumer.producerPaused,
+            },
+        };
     }
 
     closeProducer(producer_id) {
         try {
-            this.producers.get(producer_id).close()
-        } catch(e) {
-            console.warn(e)
+            this.producers.get(producer_id).close();
+        } catch (e) {
+            console.warn(e);
         }
-    
-        
-        this.producers.delete(producer_id)
+
+        this.producers.delete(producer_id);
     }
 
     getProducer(producer_id) {
-        return this.producers.get(producer_id)
+        return this.producers.get(producer_id);
     }
 
     close() {
-        this.transports.forEach(transport => transport.close())
+        this.transports.forEach((transport) => transport.close());
     }
 
     removeConsumer(consumer_id) {
-        this.consumers.delete(consumer_id)
+        this.consumers.delete(consumer_id);
     }
-
-}
+};
--- src/config.js
+++ src/config.js
@@ -1,64 +1,61 @@
-const os = require('os')
+const os = require('os');
 
 module.exports = {
     listenIp: '0.0.0.0',
     listenPort: 3016,
     sslCrt: '../ssl/cert.pem',
     sslKey: '../ssl/key.pem',
-    
+
     mediasoup: {
-      // Worker settings
-      numWorkers : Object.keys(os.cpus()).length,
-      worker: {
-        rtcMinPort: 10000,
-        rtcMaxPort: 10100,
-        logLevel: 'warn',
-        logTags: [
-          'info',
-          'ice',
-          'dtls',
-          'rtp',
-          'srtp',
-          'rtcp',
-          // 'rtx',
-          // 'bwe',
-          // 'score',
-          // 'simulcast',
-          // 'svc'
-        ],
-      },
-      // Router settings
-      router: {
-        mediaCodecs:
-          [
-            {
-              kind: 'audio',
-              mimeType: 'audio/opus',
-              clockRate: 48000,
-              channels: 2
-            },
-            {
-              kind: 'video',
-              mimeType: 'video/VP8',
-              clockRate: 90000,
-              parameters:
+        // Worker settings
+        numWorkers: Object.keys(os.cpus()).length,
+        worker: {
+            rtcMinPort: 10000,
+            rtcMaxPort: 10100,
+            logLevel: 'warn',
+            logTags: [
+                'info',
+                'ice',
+                'dtls',
+                'rtp',
+                'srtp',
+                'rtcp',
+                // 'rtx',
+                // 'bwe',
+                // 'score',
+                // 'simulcast',
+                // 'svc'
+            ],
+        },
+        // Router settings
+        router: {
+            mediaCodecs: [
                 {
-                  'x-google-start-bitrate': 1000
-                }
-            },
-          ]
-      },
-    // WebRtcTransport settings
-    webRtcTransport: {
-        listenIps: [
-          {
-            ip: '0.0.0.0',      
-            announcedIp:'127.0.0.1' // replace by public IP address
-          }
-        ],
-        maxIncomingBitrate: 1500000,
-        initialAvailableOutgoingBitrate: 1000000
+                    kind: 'audio',
+                    mimeType: 'audio/opus',
+                    clockRate: 48000,
+                    channels: 2,
+                },
+                {
+                    kind: 'video',
+                    mimeType: 'video/VP8',
+                    clockRate: 90000,
+                    parameters: {
+                        'x-google-start-bitrate': 1000,
+                    },
+                },
+            ],
+        },
+        // WebRtcTransport settings
+        webRtcTransport: {
+            listenIps: [
+                {
+                    ip: '0.0.0.0',
+                    announcedIp: '127.0.0.1', // replace by public IP address
+                },
+            ],
+            maxIncomingBitrate: 1500000,
+            initialAvailableOutgoingBitrate: 1000000,
+        },
     },
-    }
-  };
-  
+};
