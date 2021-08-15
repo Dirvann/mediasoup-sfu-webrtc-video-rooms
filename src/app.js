dd52d6c932b21add48456f93dbba19a70f7e6a0c
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2021-08-15 21:50:38 +0200

    html lint with 4 tabwidth

--- src/app.js
+++ src/app.js
@@ -110,7 +110,7 @@ io.on('connection', (socket) => {
   socket.on('getProducers', () => {
     if (!roomList.has(socket.room_id)) return
     console.log('Get producers', { name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}` })
-    
+
     // send all the current producer to newly joined member
     let producerList = roomList.get(socket.room_id).getProducerListForPeer()
 
