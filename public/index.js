6f9cfe47ab620b049a8c8de5efdd2f099ee9b4f4
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2021-08-15 18:49:27 +0200

    prettier change + removed unused dependencies
    also updated mediasoup version to 3.8

--- public/index.js
+++ public/index.js
@@ -1,148 +1,139 @@
-if (location.href.substr(0, 5) !== 'https') location.href = 'https' + location.href.substr(4, location.href.length - 4);
+if (location.href.substr(0, 5) !== 'https') location.href = 'https' + location.href.substr(4, location.href.length - 4)
 
-const socket = io();
+const socket = io()
 
-let producer = null;
+let producer = null
 
-nameInput.value = 'user_' + Math.round(Math.random() * 1000);
+nameInput.value = 'user_' + Math.round(Math.random() * 1000)
 
 socket.request = function request(type, data = {}) {
-    return new Promise((resolve, reject) => {
-        socket.emit(type, data, (data) => {
-            if (data.error) {
-                reject(data.error);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-};
+  return new Promise((resolve, reject) => {
+    socket.emit(type, data, (data) => {
+      if (data.error) {
+        reject(data.error)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
 
-let rc = null;
+let rc = null
 
 function joinRoom(name, room_id) {
-    if (rc && rc.isOpen()) {
-        console.log('Already connected to a room');
-    } else {
-        initEnumerateDevices();
+  if (rc && rc.isOpen()) {
+    console.log('Already connected to a room')
+  } else {
+    initEnumerateDevices()
 
-        rc = new RoomClient(
-            localMedia,
-            remoteVideos,
-            remoteAudios,
-            window.mediasoupClient,
-            socket,
-            room_id,
-            name,
-            roomOpen,
-        );
+    rc = new RoomClient(localMedia, remoteVideos, remoteAudios, window.mediasoupClient, socket, room_id, name, roomOpen)
 
-        addListeners();
-    }
+    addListeners()
+  }
 }
 
 function roomOpen() {
-    login.className = 'hidden';
-    reveal(startAudioButton);
-    hide(stopAudioButton);
-    reveal(startVideoButton);
-    hide(stopVideoButton);
-    reveal(startScreenButton);
-    hide(stopScreenButton);
-    reveal(exitButton);
-    reveal(copyButton);
-    reveal(devicesButton);
-    control.className = '';
-    reveal(videoMedia);
+  login.className = 'hidden'
+  reveal(startAudioButton)
+  hide(stopAudioButton)
+  reveal(startVideoButton)
+  hide(stopVideoButton)
+  reveal(startScreenButton)
+  hide(stopScreenButton)
+  reveal(exitButton)
+  reveal(copyButton)
+  reveal(devicesButton)
+  control.className = ''
+  reveal(videoMedia)
 }
 
 function hide(elem) {
-    elem.className = 'hidden';
+  elem.className = 'hidden'
 }
 
 function reveal(elem) {
-    elem.className = '';
+  elem.className = ''
 }
 
 function addListeners() {
-    rc.on(RoomClient.EVENTS.startScreen, () => {
-        hide(startScreenButton);
-        reveal(stopScreenButton);
-    });
+  rc.on(RoomClient.EVENTS.startScreen, () => {
+    hide(startScreenButton)
+    reveal(stopScreenButton)
+  })
 
-    rc.on(RoomClient.EVENTS.stopScreen, () => {
-        hide(stopScreenButton);
-        reveal(startScreenButton);
-    });
+  rc.on(RoomClient.EVENTS.stopScreen, () => {
+    hide(stopScreenButton)
+    reveal(startScreenButton)
+  })
 
-    rc.on(RoomClient.EVENTS.stopAudio, () => {
-        hide(stopAudioButton);
-        reveal(startAudioButton);
-    });
-    rc.on(RoomClient.EVENTS.startAudio, () => {
-        hide(startAudioButton);
-        reveal(stopAudioButton);
-    });
+  rc.on(RoomClient.EVENTS.stopAudio, () => {
+    hide(stopAudioButton)
+    reveal(startAudioButton)
+  })
+  rc.on(RoomClient.EVENTS.startAudio, () => {
+    hide(startAudioButton)
+    reveal(stopAudioButton)
+  })
 
-    rc.on(RoomClient.EVENTS.startVideo, () => {
-        hide(startVideoButton);
-        reveal(stopVideoButton);
-    });
-    rc.on(RoomClient.EVENTS.stopVideo, () => {
-        hide(stopVideoButton);
-        reveal(startVideoButton);
-    });
-    rc.on(RoomClient.EVENTS.exitRoom, () => {
-        hide(control);
-        hide(devicesList);
-        hide(videoMedia);
-        hide(copyButton);
-        hide(devicesButton);
-        reveal(login);
-    });
+  rc.on(RoomClient.EVENTS.startVideo, () => {
+    hide(startVideoButton)
+    reveal(stopVideoButton)
+  })
+  rc.on(RoomClient.EVENTS.stopVideo, () => {
+    hide(stopVideoButton)
+    reveal(startVideoButton)
+  })
+  rc.on(RoomClient.EVENTS.exitRoom, () => {
+    hide(control)
+    hide(devicesList)
+    hide(videoMedia)
+    hide(copyButton)
+    hide(devicesButton)
+    reveal(login)
+  })
 }
 
-let isEnumerateDevices = false;
+let isEnumerateDevices = false
 
 function initEnumerateDevices() {
-    // Many browsers, without the consent of getUserMedia, cannot enumerate the devices.
-    if (isEnumerateDevices) return;
+  // Many browsers, without the consent of getUserMedia, cannot enumerate the devices.
+  if (isEnumerateDevices) return
 
-    const constraints = {
-        audio: true,
-        video: true,
-    };
+  const constraints = {
+    audio: true,
+    video: true
+  }
 
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then((stream) => {
-            enumerateDevices();
-            stream.getTracks().forEach(function (track) {
-                track.stop();
-            });
-        })
-        .catch((err) => {
-            console.error('Access denied for audio/video: ', err);
-        });
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      enumerateDevices()
+      stream.getTracks().forEach(function (track) {
+        track.stop()
+      })
+    })
+    .catch((err) => {
+      console.error('Access denied for audio/video: ', err)
+    })
 }
 
 function enumerateDevices() {
-    // Load mediaDevice options
-    navigator.mediaDevices.enumerateDevices().then((devices) =>
-        devices.forEach((device) => {
-            let el = null;
-            if ('audioinput' === device.kind) {
-                el = audioSelect;
-            } else if ('videoinput' === device.kind) {
-                el = videoSelect;
-            }
-            if (!el) return;
+  // Load mediaDevice options
+  navigator.mediaDevices.enumerateDevices().then((devices) =>
+    devices.forEach((device) => {
+      let el = null
+      if ('audioinput' === device.kind) {
+        el = audioSelect
+      } else if ('videoinput' === device.kind) {
+        el = videoSelect
+      }
+      if (!el) return
 
-            let option = document.createElement('option');
-            option.value = device.deviceId;
-            option.innerText = device.label;
-            el.appendChild(option);
-            isEnumerateDevices = true;
-        }),
-    );
+      let option = document.createElement('option')
+      option.value = device.deviceId
+      option.innerText = device.label
+      el.appendChild(option)
+      isEnumerateDevices = true
+    })
+  )
 }
--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const https = require('httpolyglot');
-const fs = require('fs');
-const mediasoup = require('mediasoup');
-const config = require('./config');
-const path = require('path');
-const Room = require('./Room');
-const Peer = require('./Peer');
+const app = express()
+const https = require('httpolyglot')
+const fs = require('fs')
+const mediasoup = require('mediasoup')
+const config = require('./config')
+const path = require('path')
+const Room = require('./Room')
+const Peer = require('./Peer')
 
 const options = {
-    key: fs.readFileSync(path.join(__dirname, config.sslKey), 'utf-8'),
-    cert: fs.readFileSync(path.join(__dirname, config.sslCrt), 'utf-8'),
-};
+  key: fs.readFileSync(path.join(__dirname, config.sslKey), 'utf-8'),
+  cert: fs.readFileSync(path.join(__dirname, config.sslCrt), 'utf-8')
+}
 
-const httpsServer = https.createServer(options, app);
-const io = require('socket.io')(httpsServer);
+const httpsServer = https.createServer(options, app)
+const io = require('socket.io')(httpsServer)
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 httpsServer.listen(config.listenPort, () => {
-    console.log('Listening on https://' + config.listenIp + ':' + config.listenPort);
-});
+  console.log('Listening on https://' + config.listenIp + ':' + config.listenPort)
+})
 
 // all mediasoup workers
-let workers = [];
-let nextMediasoupWorkerIdx = 0;
+let workers = []
+let nextMediasoupWorkerIdx = 0
 
 /**
  * roomList
@@ -45,224 +45,220 @@ let nextMediasoupWorkerIdx = 0;
  *  }
  * }
  */
-let roomList = new Map();
+let roomList = new Map()
 
-(async () => {
-    await createWorkers();
-})();
+;(async () => {
+  await createWorkers()
+})()
 
 async function createWorkers() {
-    let { numWorkers } = config.mediasoup;
+  let { numWorkers } = config.mediasoup
 
-    for (let i = 0; i < numWorkers; i++) {
-        let worker = await mediasoup.createWorker({
-            logLevel: config.mediasoup.worker.logLevel,
-            logTags: config.mediasoup.worker.logTags,
-            rtcMinPort: config.mediasoup.worker.rtcMinPort,
-            rtcMaxPort: config.mediasoup.worker.rtcMaxPort,
-        });
+  for (let i = 0; i < numWorkers; i++) {
+    let worker = await mediasoup.createWorker({
+      logLevel: config.mediasoup.worker.logLevel,
+      logTags: config.mediasoup.worker.logTags,
+      rtcMinPort: config.mediasoup.worker.rtcMinPort,
+      rtcMaxPort: config.mediasoup.worker.rtcMaxPort
+    })
 
-        worker.on('died', () => {
-            console.error('mediasoup worker died, exiting in 2 seconds... [pid:%d]', worker.pid);
-            setTimeout(() => process.exit(1), 2000);
-        });
-        workers.push(worker);
+    worker.on('died', () => {
+      console.error('mediasoup worker died, exiting in 2 seconds... [pid:%d]', worker.pid)
+      setTimeout(() => process.exit(1), 2000)
+    })
+    workers.push(worker)
 
-        // log worker resource usage
-        /*setInterval(async () => {
+    // log worker resource usage
+    /*setInterval(async () => {
             const usage = await worker.getResourceUsage();
 
             console.info('mediasoup Worker resource usage [pid:%d]: %o', worker.pid, usage);
         }, 120000);*/
-    }
+  }
 }
 
 io.on('connection', (socket) => {
-    socket.on('createRoom', async ({ room_id }, callback) => {
-        if (roomList.has(room_id)) {
-            callback('already exists');
-        } else {
-            console.log('Created room', { room_id: room_id });
-            let worker = await getMediasoupWorker();
-            roomList.set(room_id, new Room(room_id, worker, io));
-            callback(room_id);
-        }
-    });
+  socket.on('createRoom', async ({ room_id }, callback) => {
+    if (roomList.has(room_id)) {
+      callback('already exists')
+    } else {
+      console.log('Created room', { room_id: room_id })
+      let worker = await getMediasoupWorker()
+      roomList.set(room_id, new Room(room_id, worker, io))
+      callback(room_id)
+    }
+  })
 
-    socket.on('join', ({ room_id, name }, cb) => {
-        console.log('User joined', {
-            room_id: room_id,
-            name: name,
-        });
+  socket.on('join', ({ room_id, name }, cb) => {
+    console.log('User joined', {
+      room_id: room_id,
+      name: name
+    })
 
-        if (!roomList.has(room_id)) {
-            return cb({
-                error: 'Room does not exist',
-            });
-        }
+    if (!roomList.has(room_id)) {
+      return cb({
+        error: 'Room does not exist'
+      })
+    }
 
-        roomList.get(room_id).addPeer(new Peer(socket.id, name));
-        socket.room_id = room_id;
+    roomList.get(room_id).addPeer(new Peer(socket.id, name))
+    socket.room_id = room_id
 
-        cb(roomList.get(room_id).toJson());
-    });
+    cb(roomList.get(room_id).toJson())
+  })
 
-    socket.on('getProducers', () => {
-        console.log('Get producers', { name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}` });
+  socket.on('getProducers', () => {
+    console.log('Get producers', { name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}` })
 
-        // send all the current producer to newly joined member
-        if (!roomList.has(socket.room_id)) return;
-        let producerList = roomList.get(socket.room_id).getProducerListForPeer();
+    // send all the current producer to newly joined member
+    if (!roomList.has(socket.room_id)) return
+    let producerList = roomList.get(socket.room_id).getProducerListForPeer()
 
-        socket.emit('newProducers', producerList);
-    });
+    socket.emit('newProducers', producerList)
+  })
 
-    socket.on('getRouterRtpCapabilities', (_, callback) => {
-        console.log('Get RouterRtpCapabilities', {
-            name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-        });
+  socket.on('getRouterRtpCapabilities', (_, callback) => {
+    console.log('Get RouterRtpCapabilities', {
+      name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}`
+    })
 
-        try {
-            callback(roomList.get(socket.room_id).getRtpCapabilities());
-        } catch (e) {
-            callback({
-                error: e.message,
-            });
-        }
-    });
+    try {
+      callback(roomList.get(socket.room_id).getRtpCapabilities())
+    } catch (e) {
+      callback({
+        error: e.message
+      })
+    }
+  })
 
-    socket.on('createWebRtcTransport', async (_, callback) => {
-        console.log('Create webrtc transport', {
-            name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-        });
+  socket.on('createWebRtcTransport', async (_, callback) => {
+    console.log('Create webrtc transport', {
+      name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}`
+    })
 
-        try {
-            const { params } = await roomList.get(socket.room_id).createWebRtcTransport(socket.id);
+    try {
+      const { params } = await roomList.get(socket.room_id).createWebRtcTransport(socket.id)
 
-            callback(params);
-        } catch (err) {
-            console.error(err);
-            callback({
-                error: err.message,
-            });
-        }
-    });
+      callback(params)
+    } catch (err) {
+      console.error(err)
+      callback({
+        error: err.message
+      })
+    }
+  })
 
-    socket.on('connectTransport', async ({ transport_id, dtlsParameters }, callback) => {
-        console.log('Connect transport', { name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}` });
+  socket.on('connectTransport', async ({ transport_id, dtlsParameters }, callback) => {
+    console.log('Connect transport', { name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}` })
 
-        if (!roomList.has(socket.room_id)) return;
-        await roomList.get(socket.room_id).connectPeerTransport(socket.id, transport_id, dtlsParameters);
+    if (!roomList.has(socket.room_id)) return
+    await roomList.get(socket.room_id).connectPeerTransport(socket.id, transport_id, dtlsParameters)
 
-        callback('success');
-    });
+    callback('success')
+  })
 
-    socket.on('produce', async ({ kind, rtpParameters, producerTransportId }, callback) => {
-        if (!roomList.has(socket.room_id)) {
-            return callback({ error: 'not is a room' });
-        }
+  socket.on('produce', async ({ kind, rtpParameters, producerTransportId }, callback) => {
+    if (!roomList.has(socket.room_id)) {
+      return callback({ error: 'not is a room' })
+    }
 
-        let producer_id = await roomList
-            .get(socket.room_id)
-            .produce(socket.id, producerTransportId, rtpParameters, kind);
+    let producer_id = await roomList.get(socket.room_id).produce(socket.id, producerTransportId, rtpParameters, kind)
 
-        console.log('Produce', {
-            type: `${kind}`,
-            name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-            id: `${producer_id}`,
-        });
+    console.log('Produce', {
+      type: `${kind}`,
+      name: `${roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
+      id: `${producer_id}`
+    })
 
-        callback({
-            producer_id,
-        });
-    });
+    callback({
+      producer_id
+    })
+  })
 
-    socket.on('consume', async ({ consumerTransportId, producerId, rtpCapabilities }, callback) => {
-        //TODO null handling
-        let params = await roomList
-            .get(socket.room_id)
-            .consume(socket.id, consumerTransportId, producerId, rtpCapabilities);
+  socket.on('consume', async ({ consumerTransportId, producerId, rtpCapabilities }, callback) => {
+    //TODO null handling
+    let params = await roomList.get(socket.room_id).consume(socket.id, consumerTransportId, producerId, rtpCapabilities)
 
-        console.log('Consuming', {
-            name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-            producer_id: `${producerId}`,
-            consumer_id: `${params.id}`,
-        });
+    console.log('Consuming', {
+      name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
+      producer_id: `${producerId}`,
+      consumer_id: `${params.id}`
+    })
 
-        callback(params);
-    });
+    callback(params)
+  })
 
-    socket.on('resume', async (data, callback) => {
-        await consumer.resume();
-        callback();
-    });
+  socket.on('resume', async (data, callback) => {
+    await consumer.resume()
+    callback()
+  })
 
-    socket.on('getMyRoomInfo', (_, cb) => {
-        cb(roomList.get(socket.room_id).toJson());
-    });
+  socket.on('getMyRoomInfo', (_, cb) => {
+    cb(roomList.get(socket.room_id).toJson())
+  })
 
-    socket.on('disconnect', () => {
-        console.log('Disconnect', {
-            name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-        });
+  socket.on('disconnect', () => {
+    console.log('Disconnect', {
+      name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`
+    })
 
-        if (!socket.room_id) return;
-        roomList.get(socket.room_id).removePeer(socket.id);
-    });
+    if (!socket.room_id) return
+    roomList.get(socket.room_id).removePeer(socket.id)
+  })
 
-    socket.on('producerClosed', ({ producer_id }) => {
-        console.log('Producer close', {
-            name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-        });
+  socket.on('producerClosed', ({ producer_id }) => {
+    console.log('Producer close', {
+      name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`
+    })
 
-        roomList.get(socket.room_id).closeProducer(socket.id, producer_id);
-    });
+    roomList.get(socket.room_id).closeProducer(socket.id, producer_id)
+  })
 
-    socket.on('exitRoom', async (_, callback) => {
-        console.log('Exit room', {
-            name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`,
-        });
+  socket.on('exitRoom', async (_, callback) => {
+    console.log('Exit room', {
+      name: `${roomList.get(socket.room_id) && roomList.get(socket.room_id).getPeers().get(socket.id).name}`
+    })
 
-        if (!roomList.has(socket.room_id)) {
-            callback({
-                error: 'not currently in a room',
-            });
-            return;
-        }
-        // close transports
-        await roomList.get(socket.room_id).removePeer(socket.id);
-        if (roomList.get(socket.room_id).getPeers().size === 0) {
-            roomList.delete(socket.room_id);
-        }
+    if (!roomList.has(socket.room_id)) {
+      callback({
+        error: 'not currently in a room'
+      })
+      return
+    }
+    // close transports
+    await roomList.get(socket.room_id).removePeer(socket.id)
+    if (roomList.get(socket.room_id).getPeers().size === 0) {
+      roomList.delete(socket.room_id)
+    }
 
-        socket.room_id = null;
+    socket.room_id = null
 
-        callback('successfully exited room');
-    });
-});
+    callback('successfully exited room')
+  })
+})
 
 // TODO remove - never used?
 function room() {
-    return Object.values(roomList).map((r) => {
+  return Object.values(roomList).map((r) => {
+    return {
+      router: r.router.id,
+      peers: Object.values(r.peers).map((p) => {
         return {
-            router: r.router.id,
-            peers: Object.values(r.peers).map((p) => {
-                return {
-                    name: p.name,
-                };
-            }),
-            id: r.id,
-        };
-    });
+          name: p.name
+        }
+      }),
+      id: r.id
+    }
+  })
 }
 
 /**
  * Get next mediasoup Worker.
  */
 function getMediasoupWorker() {
-    const worker = workers[nextMediasoupWorkerIdx];
+  const worker = workers[nextMediasoupWorkerIdx]
 
-    if (++nextMediasoupWorkerIdx === workers.length) nextMediasoupWorkerIdx = 0;
+  if (++nextMediasoupWorkerIdx === workers.length) nextMediasoupWorkerIdx = 0
 
-    return worker;
+  return worker
 }
