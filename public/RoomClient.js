dd5c15c2d6db4bfd0dbe18fd99f4a1844c1617a7
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-08-12 13:48:28 +0200

    Start video-audio on join room

--- public/RoomClient.js
+++ public/RoomClient.js
@@ -86,6 +86,8 @@ class RoomClient {
           this.device = device
           await this.initTransports(device)
           this.socket.emit('getProducers')
+          startVideoButton.click()
+          startAudioButton.click()
         }.bind(this)
       )
       .catch((err) => {
